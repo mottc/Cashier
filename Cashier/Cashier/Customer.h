6788cc463a245bd8a5a9603fa0e7fc5589d7445d
mottc <mottcode@163.com>
2017-09-18 18:51:08 +0800

    增加Customer类头文件字段及其get,set方法

--- Cashier/Cashier/Customer.h
+++ Cashier/Cashier/Customer.h
@@ -1,1 +1,41 @@
 #pragma once
+
+#include <string>
+
+using namespace std;
+
+class Customer
+{
+public:
+	Customer();
+	Customer(string name, int age, double balance);
+	~Customer();
+
+private:
+	string name;
+	int age;
+	double balance;
+
+public:
+	string getName();
+	void setName(string name);
+	int getAge();
+	void setAge(int age);
+	double getBalance();
+	void setBalance();
+	void cost(double cost);
+	void deposit(double deposit);
+};
+
+Customer::Customer()
+{
+}
+Customer::Customer(string name, int age, double balance)
+{
+	this->name = name;
+	this->age = age;
+	this->balance = balance;
+}
+Customer::~Customer()
+{
+}
