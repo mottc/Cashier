f10b915b9a5324754858f041070aa77b44d52966
mottc <mottcode@163.com>
2017-09-20 10:22:30 +0800

    perfect man-machine interface

--- Cashier/Cashier/Cashier.cpp
+++ Cashier/Cashier/Cashier.cpp
@@ -3,55 +3,104 @@
 
 using namespace std;
 
-int main()
+
+void modifyInformation(Customer* customer)
 {
-	cout << "请刷卡！" << endl;
-	getchar();
-	Customer customer("王某某", 18, 452.36);
-	cout << "*************************" << endl;
-	cout << "  姓名：" << customer.getName() << endl;
-	cout << "  年龄：" << customer.getAge() << endl;
-	cout << "  余额：" << customer.getBalance() << endl;
-	cout << "*************************" << endl << endl;
-
-	cout << "请选择您要进行的操作：" << endl;
-	cout << "1：消费" << endl;
-	cout << "2：充值" << endl << endl;
-
-	bool inputAgain = false;
+	cout << "请选择要修改的内容：" << endl;
+	cout << "0：退出修改" << endl;
+	cout << "1：姓名" << endl;
+	cout << "2：年龄" << endl;
+	bool chooseAgain = false;
 	do
 	{
-		int choice;
-		cin >> choice;
-		switch (choice)
+		int index;
+		cin >> index;
+		switch (index)
 		{
-		case 1:
-			cout << "请输入消费金额：";
-			double cost;
-			cin >> cost;
-			customer.cost(cost);
+		case 0:
 			break;
+		case 1:
+		{
+			cout << "请输入更正后的姓名：";
+			string name;
+			cin >> name;
+			customer->setName(name);
+			break;
+		}
 		case 2:
-			cout << "请输入充值金额：";
-			double deposit;
-			cin >> deposit;
-			customer.deposit(deposit);
+			cout << "请输入更正后的年龄：";
+			int age;
+			cin >> age;
+			customer->setAge(age);
 			break;
 		default:
 			cout << "您的操作有误，请重新输入！" << endl;
-			inputAgain = true;
+			chooseAgain = true;
 			break;
 		}
-	} while (inputAgain);
+	} while (chooseAgain);
 	
+}
 
-	cout << endl << "更新后信息：" << endl;
+int main()
+{
+	cout << "请刷卡！（按下任意字符键表示刷卡）" << endl;
+	getchar();
+	Customer customer("王某某", 18, 452.36);
+	cout << endl << "成功读取卡内信息：" << endl;
 	cout << "*************************" << endl;
 	cout << "  姓名：" << customer.getName() << endl;
 	cout << "  年龄：" << customer.getAge() << endl;
 	cout << "  余额：" << customer.getBalance() << endl;
 	cout << "*************************" << endl << endl;
+	while (true)
+	{
+		cout << "请选择您要进行的操作：" << endl;
+		cout << "0：退出" << endl;
+		cout << "1：消费" << endl;
+		cout << "2：充值" << endl;
+		cout << "3：修改基本信息" << endl << endl;
+		int choice;
+		cin >> choice;
+		bool inputAgain = false;
+		do
+		{
+			switch (choice)
+			{
+			case 0:
+				break;
+			case 1:
+				cout << "请输入消费金额：";
+				double cost;
+				cin >> cost;
+				customer.cost(cost);
+				break;
+			case 2:
+				cout << "请输入充值金额：";
+				double deposit;
+				cin >> deposit;
+				customer.deposit(deposit);
+				break;
+			case 3:
+				modifyInformation(&customer);
+				break;
+			default:
+				cout << "您的操作有误，请重新输入！" << endl;
+				inputAgain = true;
+				break;
+			}
+		} while (inputAgain);
 
-	system("pause");
+		if (choice == 0)
+			break;
+
+		cout << endl << "更新后信息：" << endl;
+		cout << "*************************" << endl;
+		cout << "  姓名：" << customer.getName() << endl;
+		cout << "  年龄：" << customer.getAge() << endl;
+		cout << "  余额：" << customer.getBalance() << endl;
+		cout << "*************************" << endl << endl;
+	}
+	
 	return 0;
 }
