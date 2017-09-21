cfa27a4622addeaf3c4fcd5a7f903168e769a82f
mottc <mottcode@163.com>
2017-09-21 18:26:23 +0800

    add SQLite

--- Cashier/Cashier/Cashier.cpp
+++ Cashier/Cashier/Cashier.cpp
@@ -1,8 +1,10 @@
 #include <iostream>
+#include <sstream>
 #include "Customer.h"
+#include "sqlite3.h"  
+#pragma comment(lib, "sqlite3.lib") 
 
 using namespace std;
-
 
 void modifyInformation(Customer* customer)
 {
@@ -42,10 +44,54 @@ void modifyInformation(Customer* customer)
 	
 }
 
-int main()
+int customerRegister(sqlite3 *db)
+{
+	int rc;
+	
+	char *zErrMsg = 0;
+	string name;
+	int age;
+	double balance;
+	cout << "请输入新用户姓名：";
+	cin >> name;
+	cout << "请输入新用户年龄：";
+	cin >> age;
+	cout << "请输入新用户初始金额：";
+	cin >> balance;
+
+	stringstream age_stream;
+	age_stream << age;
+	string age_string = age_stream.str();
+	stringstream balance_stream;
+	balance_stream << balance;
+	string balance_string = balance_stream.str();
+
+
+	/* Create SQL statement */
+	string sql_string = "INSERT INTO CUSTOMERS (ID,NAME,AGE,BALANCE) VALUES (null, \"" + name + "\", " + age_string + ", " + balance_string + ");";
+	cout << sql_string;
+	const char* sql = sql_string.c_str();
+	/*sprintf(sql, "INSERT INTO CUSTOMERS (ID,NAME,AGE,BALANCE)" \
+		"VALUES (null, '%s', %d, %f );", name, age, balance);*/
+	
+	/* Execute SQL statement */
+	rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
+	if (rc != SQLITE_OK) {
+		fprintf(stderr, "SQL error: %s\n", zErrMsg);
+		sqlite3_free(zErrMsg);
+	}
+	else {
+		fprintf(stdout, "新用户注册成功！\n");
+	}
+	cout << "新用户编号为：" << sqlite3_last_insert_rowid(db) << endl << endl;
+	return 0;
+}
+
+int useCard()
 {
 	cout << "请刷卡！（按下任意字符键表示刷卡）" << endl;
-	getchar();
+	char ch;
+	cin >> ch;
 	Customer customer("王某某", 18, 452.36);
 	cout << endl << "成功读取卡内信息：" << endl;
 	cout << "*************************" << endl;
@@ -101,6 +147,59 @@ int main()
 		cout << "  余额：" << customer.getBalance() << endl;
 		cout << "*************************" << endl << endl;
 	}
-	
 	return 0;
+}
+
+void main()
+{
+	sqlite3 * db;
+	int res = sqlite3_open("customer.db", &db);
+	if (res != SQLITE_OK) 
+	{
+		cout << "数据库打开失败！" << sqlite3_errmsg(db) << endl;
+	}
+	else 
+	{
+		while (true)
+		{
+			cout << "请选择要执行的操作：" << endl;
+			cout << "0：退出" << endl;
+			cout << "1：注册新用户" << endl;
+			cout << "2：用户刷卡" << endl;
+			bool again = false;
+			int index;
+			do
+			{
+				cin >> index;
+				switch (index)
+				{
+				case 0:
+					/*sql = "CREATE TABLE CUSTOMERS("  \
+						"ID INT PRIMARY KEY," \
+						"NAME           TEXT    NOT NULL," \
+						"AGE            INT     NOT NULL," \
+						"BALANCE         REAL );";
+					sqlite3_exec(db, sql, 0, 0, &zErrMsg);*/
+					break;
+				case 1:
+				{
+					customerRegister(db);
+					break;
+				}
+				case 2:
+					useCard();
+					break;
+				default:
+					cout << "您的操作有误，请重新输入！" << endl;
+					again = true;
+					break;
+				}
+			} while (again);
+			if (index == 0)
+			{
+				break;
+			}
+		}
+	}
+	sqlite3_close(db);
 }
