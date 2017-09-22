22aca99a13df31adf9a85d2dfab5905ca7b6c0ae
mottc <mottcode@163.com>
2017-09-22 21:55:07 +0800

    向数据库插入数据，从数据库查询数据

--- Cashier/Cashier/Cashier.cpp
+++ Cashier/Cashier/Cashier.cpp
@@ -1,10 +1,41 @@
 #include <iostream>
 #include <sstream>
 #include "Customer.h"
-#include "sqlite3.h"  
+#include "sqlite3.h" 
+#include "SQLiteHelper.h"
 #pragma comment(lib, "sqlite3.lib") 
 
 using namespace std;
+
+
+//utf-8转换到GB3212  
+char* U2G(const char* utf8)
+{
+	int len = MultiByteToWideChar(CP_UTF8, 0, utf8, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_UTF8, 0, utf8, -1, wstr, len);
+	len = WideCharToMultiByte(CP_ACP, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* str = new char[len + 1];
+	memset(str, 0, len + 1);
+	WideCharToMultiByte(CP_ACP, 0, wstr, -1, str, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return str;
+}
+//GB2312到UTF-8的转换  
+char* G2U(const char* gb2312)
+{
+	int len = MultiByteToWideChar(CP_ACP, 0, gb2312, -1, NULL, 0);
+	wchar_t* wstr = new wchar_t[len + 1];
+	memset(wstr, 0, len + 1);
+	MultiByteToWideChar(CP_ACP, 0, gb2312, -1, wstr, len);
+	len = WideCharToMultiByte(CP_UTF8, 0, wstr, -1, NULL, 0, NULL, NULL);
+	char* str = new char[len + 1];
+	memset(str, 0, len + 1);
+	WideCharToMultiByte(CP_UTF8, 0, wstr, -1, str, len, NULL, NULL);
+	if (wstr) delete[] wstr;
+	return str;
+}
 
 void modifyInformation(Customer* customer)
 {
@@ -41,17 +72,14 @@ void modifyInformation(Customer* customer)
 			break;
 		}
 	} while (chooseAgain);
-	
 }
 
-int customerRegister(sqlite3 *db)
+void customerRegister(SQLiteHelper *sqliteHelper)
 {
 	int rc;
-	
-	char *zErrMsg = 0;
 	string name;
-	int age;
-	double balance;
+	string age;
+	string balance;
 	cout << "请输入新用户姓名：";
 	cin >> name;
 	cout << "请输入新用户年龄：";
@@ -59,39 +87,98 @@ int customerRegister(sqlite3 *db)
 	cout << "请输入新用户初始金额：";
 	cin >> balance;
 
-	stringstream age_stream;
-	age_stream << age;
-	string age_string = age_stream.str();
-	stringstream balance_stream;
-	balance_stream << balance;
-	string balance_string = balance_stream.str();
-
-
 	/* Create SQL statement */
-	string sql_string = "INSERT INTO CUSTOMERS (ID,NAME,AGE,BALANCE) VALUES (null, \"" + name + "\", " + age_string + ", " + balance_string + ");";
+	string sql_string = "INSERT INTO CUSTOMERS (ID,NAME,AGE,BALANCE) VALUES (null, \"" + name + "\", " + age + ", " + balance + ");";
+	const char* sql1 = sql_string.c_str();
+	char* sql = G2U(sql1);
+	/* Execute SQL statement */
+	rc = sqliteHelper->execSQL(sql);
+	if (rc != SQLITE_OK) {
+		cout << "SQL error!" << endl;
+	}
+	else {
+		cout << "新用户注册成功！" << endl;
+	}
+	cout << "新用户编号为：" << sqliteHelper->getLastID() << endl << endl;
+}
+
+static int callback(void *data, int argc, char **argv, char **azColName, int a) {
+	int i;
+	fprintf(stderr, "%s: ", (const char*)data);
+	for (i = 0; i<argc; i++) {
+		a = 99;
+		printf("%s = %s\n", azColName[i], argv[i] ? argv[i] : "NULL");
+	}
+	printf("\n");
+	return 0;
+}
+void useCard(SQLiteHelper *sqliteHelper)
+{
+	cout << "请刷卡！（输入ID，表示从卡中读取的数据）" << endl;
+	string ID;
+	cin >> ID;
+	string sql_string = "SELECT NAME,AGE,BALANCE FROM CUSTOMERS WHERE ID=" + ID;
 	cout << sql_string;
 	const char* sql = sql_string.c_str();
-	/*sprintf(sql, "INSERT INTO CUSTOMERS (ID,NAME,AGE,BALANCE)" \
-		"VALUES (null, '%s', %d, %f );", name, age, balance);*/
-	
-	/* Execute SQL statement */
-	rc = sqlite3_exec(db, sql, 0, 0, &zErrMsg);
+	//char *Initialization = "Init";
+	//char** pResult = &Initialization;
+	//int nRow;
+	//int nCol;
+	//int nResult;
+	sqlite3 *db;
+	char *zErrMsg = 0;
+	int rc;
+	//char *sql;
+	const char* data = "Callback function called";
+
+	/* Open database */
+	rc = sqlite3_open("customer.db", &db);
+	if (rc) {
+		fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db));
+		exit(0);
+	}
+	else {
+		fprintf(stderr, "Opened database successfully\n");
+	}
+
+	/* Create SQL statement */
+	//sql = "SELECT * from COMPANY";
+
+	rc = sqlite3_exec(db, sql, callback, (void*)data, &zErrMsg);
 	if (rc != SQLITE_OK) {
 		fprintf(stderr, "SQL error: %s\n", zErrMsg);
 		sqlite3_free(zErrMsg);
 	}
 	else {
-		fprintf(stdout, "新用户注册成功！\n");
+		fprintf(stdout, "Operation done successfully\n");
 	}
-	cout << "新用户编号为：" << sqlite3_last_insert_rowid(db) << endl << endl;
-	return 0;
-}
 
-int useCard()
-{
-	cout << "请刷卡！（按下任意字符键表示刷卡）" << endl;
-	char ch;
-	cin >> ch;
+	sqlite3_close(db);
+	//nResult = sqliteHelper->rawQuery(sql, &nRow, &nCol, pResult);
+	//nResult = sqlite3_get_table(db, strSql.c_str(), &pResult, &nRow, &nCol, &errmsg);
+	/*if (nResult != SQLITE_OK)
+	{
+		cout << "对不起，没有该ID对应的顾客。请确认您的输入！";
+		return;
+	}
+	cout << endl << nRow << nCol;
+	char* rrr = U2G(pResult[5]);
+	cout << rrr;*/
+	//string strOut;
+	//int index = nCol;
+	//for (int i = 0; i < nRow; i++)
+	//{
+	//	printf("第 %d 条记录\n", i);
+	//	for (int j = 0; j < nCol; j++)
+	//	{
+	//		printf("字段名:%s ? > 字段值:%s\n", pResult[5], pResult[5]);
+	//		index++;
+	//		// dbResult 的字段值是连续的，从第0索引到第 nColumn - 1索引都是字段名称，从第 nColumn 索引开始，后面都是字段值，它把一个二维的表（传统的行列表示法）用一个扁平的形式来表示
+	//	}
+	//}
+
+
+
 	Customer customer("王某某", 18, 452.36);
 	cout << endl << "成功读取卡内信息：" << endl;
 	cout << "*************************" << endl;
@@ -147,59 +234,70 @@ int useCard()
 		cout << "  余额：" << customer.getBalance() << endl;
 		cout << "*************************" << endl << endl;
 	}
-	return 0;
+}
+
+int operate(SQLiteHelper *sqliteHelper)
+{
+	cout << "请选择要执行的操作：" << endl;
+	cout << "0：退出" << endl;
+	cout << "1：注册新用户" << endl;
+	cout << "2：用户刷卡" << endl;
+
+	/*sqlite3 *db;
+	char *sql;
+
+	sqlite3_open("customer.db", &db);*/
+	bool again = false;
+	int index;
+	do
+	{
+		cin >> index;
+		switch (index)
+		{
+		case 0:
+			/*sql = "CREATE TABLE CUSTOMERS("  \
+			"ID INTEGER PRIMARY KEY AUTOINCREMENT," \
+			"NAME TEXT NOT NULL," \
+			"AGE INT NOT NULL," \
+			"BALANCE REAL);";
+			sqlite3_exec(db, sql, 0, 0, 0);*/
+			break;
+		case 1:
+		{
+			customerRegister(sqliteHelper);
+			break;
+		}
+		case 2:
+			useCard(sqliteHelper);
+			break;
+		default:
+			cout << "您的操作有误，请重新输入！" << endl;
+			again = true;
+			break;
+		}
+	} while (again);
+	return index;
 }
 
 void main()
 {
-	sqlite3 * db;
-	int res = sqlite3_open("customer.db", &db);
-	if (res != SQLITE_OK) 
-	{
-		cout << "数据库打开失败！" << sqlite3_errmsg(db) << endl;
-	}
-	else 
+	SQLiteHelper *sqliteHelper = new SQLiteHelper();
+	int res = sqliteHelper->openDB("customer.db");
+	if (res == SQLITE_OK) 
 	{
 		while (true)
 		{
-			cout << "请选择要执行的操作：" << endl;
-			cout << "0：退出" << endl;
-			cout << "1：注册新用户" << endl;
-			cout << "2：用户刷卡" << endl;
-			bool again = false;
-			int index;
-			do
-			{
-				cin >> index;
-				switch (index)
-				{
-				case 0:
-					/*sql = "CREATE TABLE CUSTOMERS("  \
-						"ID INT PRIMARY KEY," \
-						"NAME           TEXT    NOT NULL," \
-						"AGE            INT     NOT NULL," \
-						"BALANCE         REAL );";
-					sqlite3_exec(db, sql, 0, 0, &zErrMsg);*/
-					break;
-				case 1:
-				{
-					customerRegister(db);
-					break;
-				}
-				case 2:
-					useCard();
-					break;
-				default:
-					cout << "您的操作有误，请重新输入！" << endl;
-					again = true;
-					break;
-				}
-			} while (again);
-			if (index == 0)
+			int result = operate(sqliteHelper);
+			if (result == 0)
 			{
 				break;
 			}
 		}
 	}
-	sqlite3_close(db);
+	else
+	{
+		cout << "数据库打开失败！" << endl;
+	}
+	sqliteHelper->closeDB();
+	getchar();
 }
